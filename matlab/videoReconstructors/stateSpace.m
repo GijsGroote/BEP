61ceb257cb728d36124c32cd74cd1d376e60a9f3
ttveen <ttveen@gmail.com>
2019-05-02 14:03:46 +0200

    kan door kamlman_filter_simplified heen loopen

--- matlab/videoReconstructors/stateSpace.m
+++ matlab/videoReconstructors/stateSpace.m
@@ -38,19 +38,19 @@ state = struct('state', zeros(vectorLength, 1, 3, 'double')); % initializing the
 % (P_j)^- = aPrioriCov[n, n]:
 % P_j = aPosterioriCov[n, n]:
 % K_j = kalmanFilterGain[n, m]:
-
+s(1).x = reconstructedFrame;
+s(1).P = ones(360,450);
+s(1).Q = 1;
+s(1).R = 2;
 state(1).vector = reshape(reconstructedFrame, vectorLength, 1, 3); % reshape first frame to a vector
 %% loop over every frame
 for k = 2:k_end 
     state(k).state =  reshape(faultyVideo(k).frame, vectorLength, 1, 3); % reshape faulty video to vector
     % loop over 3 basis colors
-    for rgb = 1 : 3         
+    s(k) = Kalman_filter_simplified(s(k-1), im2double(faultyVideo(k).frame), nColumns, nRows);
     
-
-
-        
-        
-        kalman_filter (state(k),frameWidth, frameHeight)
-    end
+%     for rgb = 1 : 3         
+%         kalman_filter (state(k),frameWidth, frameHeight)
+%     end
 end
 end
