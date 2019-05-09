95283d2c7a26a5e2d7680120c9df1fe3ce95d259
Maarten ten Voorde <maartentenvoorde1998@gmail.com>
2019-05-09 11:33:51 +0200

    comments toegevoegd en muTotal wordt nu ook berekend

--- matlab/videoReconstructors/calculateProcessNoiceAndCovariance.m
+++ matlab/videoReconstructors/calculateProcessNoiceAndCovariance.m
@@ -1,8 +1,5 @@
-function [processNoise, covMatrix, mu] = calculateProcessNoiceAndCovariance(structVideo)
+function [processNoise, covMatrix, mu, muTotal] = calculateProcessNoiceAndCovariance(structVideo)
 % this function calculates the process noice and covariance matrix.
-
-% FUTURE TODO: mu is there to check, it should approach 0. It is here to check if it actually goes to 0
-% addtional expension: loop over all the frames for more accuracyyyyy
 
 % INPUT
 % structVideo   Struct of the original video
@@ -11,7 +8,6 @@ function [processNoise, covMatrix, mu] = calculateProcessNoiceAndCovariance(stru
 % processNoise  Frame 2 = frame 1 + process noice
 % covMatrix     The covariance matrix of the process noice
 % mu            Validate if code does what is supposed to do, approach 0
-
 
 %% setup
 frame1 = double(structVideo(1).cdata);
@@ -29,9 +25,29 @@ mu = [0, 0, 0];
 processNoise = frame2 - frame1;
 
 %% create covariance matrix. And calculate average of every color frame for 3 colors
+
 for k = 1:3
     covMatrix(:,:,k) = cov(processNoise(:,:,k));
     mu(k) = mean(processNoise(:,:,k),'all');
 end
+
+muSum = [0,0,0];
+
+% Uncomment to view plot of the average of the processNoise 
+% figure;
+% hold on;
+
+for i=1:length(structVideo)-1
+    processNoise = double(structVideo(i+1).cdata)-double(structVideo(i).cdata);
+    muSum = muSum + mean(processNoise, 'all');
+    
+% This is also needed for the plot of the average of the ProcessNoise
+%     gem = muSum/i;
+%     plot(i,gem(1),'-s')
+end
+
+% muTotal should approach zero
+muTotal = muSum/i;
+
 end
 
