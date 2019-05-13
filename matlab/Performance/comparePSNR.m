c122e12aeee90b9c5084cab986d62030f701da4c
Willem <willem@icch.nl>
2019-05-13 11:35:28 +0200

    Ik ben aan de slag voor de file voor de PSNR

---
+++ matlab/Performance/comparePSNR.m
@@ -0,0 +1,23 @@
+function [outputArg1,outputArg2] = comparePSNR(inputArg1,inputArg2)
+%This function calculates the peak signal-to-noise ratio (PSNR) for the unfiltered
+%and filtered version compared to the original video
+
+%INPUT
+%originalVideo                 the original video in struct uint-8 format
+%reconstructedVideo            the reconstructed video in struct uint-8 format
+%reconstructedFilteredVideo    the reconstructed and filtered video in
+%struct uint-8 format
+
+%OUTPUT
+% TODO
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+
+%% Calculate Mean Squared Error (MSR)
+
+
+
+%% Calculcate PSNR
+
+
+end
