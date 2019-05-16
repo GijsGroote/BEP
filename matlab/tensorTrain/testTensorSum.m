b9446561ac14255b18b0d23be832c1516e15940f
Gijs Groote <g.s.groote@student.tudelft.nl>
2019-05-16 09:11:41 +0200

    deleted .asv file

--- matlab/tensorTrain/testTensorSum.m
+++ matlab/tensorTrain/testTensorSum.m
@@ -18,12 +18,12 @@ function testOnesAndZeros(testCase)
     w1 = [1 0 ; 1 1];
     p2 = [0 0 0 ; 1 1 1 ; 0 0 0];
     w2 = [1 0 1 ; 1 0 1 ; 1 0 1];
-
+    
     % act, use int64 to round matrix to integer values
     % test could fail on calculation errors smaller than 10^-5 
     actualSolution = int64(tensorSum(p1, p2, w1, w2));
     expectedSolution = int64([ -2.3440   -1.2247 ; -3.5688   -2.3440]);
-        
+    
     % assert
     assertTrue(testCase, isequal(actualSolution, expectedSolution));
 end
